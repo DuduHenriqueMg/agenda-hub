85efa881170e8af7dae6b1bdb183539290153193
Eduardo Henrique <dudugama03@gmail.com>
2026-03-05 02:28:58 -0300

    fix(tenant): cria migration do tenant, adicona tenant_id e role em users e muda as migrations iniciais para rodar a migration do tenant primeiro

--- database/migrations/0001_01_01_000001_create_users_table.php
+++ database/migrations/0001_01_01_000001_create_users_table.php
@@ -15,10 +15,14 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('role');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->unsignedBigInteger('tenant_id');
+
+             $table->foreign('tenant_id')->references('id')->on('tenants');
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
